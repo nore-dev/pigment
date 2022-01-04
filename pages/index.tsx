cd026dede17a30b2d8781e3ef02349dec98ec840
noredev <noredeveloper@gmail.com>
2022-01-04 23:50:25 +0300

    fix: remove css imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,8 @@
-import { Box } from '@chakra-ui/react'
-import { PrismaClient } from '@prisma/client'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-
-
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <p>hello world</p>
     </div>
   )
